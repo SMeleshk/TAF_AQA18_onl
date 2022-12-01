8d27caefef7586da82a16e65c1c4d18a7142969d
SMeleshk <svetla.meleshko@gmail.com>
2022-12-01 20:59:35 +0300

    1st and 2nd classes on AutoTesting

---
+++ src/test/java/tests/GroupsTest.java
@@ -0,0 +1,33 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GroupsTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test (groups = "smoke")
+    public void stepb(){
+        Assert.assertEquals(calculator.sum(2, 3), 5, "неверная сумма");
+    }
+
+    @Test (groups = "regression")
+    public void stepa(){
+        Assert.assertEquals(calculator.sum(2, 3), 6, "неверная сумма");
+    }
+
+    @Test  (groups = "regression")
+    public void stepd(){
+        Assert.assertEquals(calculator.sum(2, 3), 5, "неверная сумма");
+    }
+
+    @Test (groups = {"smoke", "regression"})
+    public void stepc1(){
+        Assert.assertEquals(calculator.sum(2, 3), 5, "неверная сумма");
+    }
+
+
+
+
+}
