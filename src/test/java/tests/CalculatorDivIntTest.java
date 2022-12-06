5cbeefb5e17a027bc75884e031a7c16fe4e2a13a
SMeleshk <svetla.meleshko@gmail.com>
2022-12-06 22:21:05 +0300

    changes according to the request

--- src/test/java/tests/CalculatorDivIntTest.java
+++ src/test/java/tests/CalculatorDivIntTest.java
@@ -13,42 +13,42 @@ public class CalculatorDivIntTest extends PreAndPostConditions {
     }
 
     @Test (testName = "both positive", description = "priority 1", priority = 1)
-    public void testDivision2(){
+    public void testDivision2() {
         Assert.assertEquals(calculator.divInt(6, 3), 2, "неверное частное");
     }
 
     @Test (testName = "|a|<|b|")
-    public void testDivision3(){
+    public void testDivision3() {
         Assert.assertEquals(calculator.divInt(3, 6), 0, "неверное частное");
     }
 
     @Test (testName = "a negative, b positive")
-    public void testDivision4(){
+    public void testDivision4() {
         Assert.assertEquals(calculator.divInt(-6, 3), -2, "неверное частное");
     }
 
     @Test (testName = "a positive, b negative")
-    public void testDivision5(){
+    public void testDivision5() {
         Assert.assertEquals(calculator.divInt(6, -3), -2, "неверное частное");
     }
 
     @Test (testName = "both negative")
-    public void testDivision6(){
+    public void testDivision6() {
         Assert.assertEquals(calculator.divInt(-6, -3), 2, "неверное частное");
     }
 
     @Test (testName = "invocation x3", invocationCount = 3)
-    public void testDivision7(){
+    public void testDivision7() {
         Assert.assertEquals(calculator.divInt(-6, -1), 6, "неверное частное");
     }
 
     @Test (testName = "min and max", invocationCount = 3, invocationTimeOut = 30, threadPoolSize = 3) //min-1 и max+1 idea не позволяет сделать
-    public void testDivision8(){
+    public void testDivision8() {
         Assert.assertEquals(calculator.divInt(-2147483648, 2147483647), -1, "неверное частное");
     }
 
     @Test (testName = "max and min", timeOut = 100)
-    public void testDivision9(){
+    public void testDivision9() {
         Assert.assertEquals(calculator.divInt(2147483647, -2147483648), 0, "неверное частное");
     }
 
