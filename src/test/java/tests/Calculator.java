d239a6eab1ad60412e533ec9e1c957a0aad41256
SMeleshk <svetla.meleshko@gmail.com>
2022-12-18 01:02:59 +0300

    HW new version

--- src/test/java/tests/Calculator.java
+++ src/test/java/tests/Calculator.java
@@ -6,11 +6,11 @@ public class Calculator {
         return a + b;
     }
 
-    public int divInt (int a, int b) {
+    public int division (int a, int b) {
         return a / b;
     }
 
-    public double divDouble (double a, double b) {
+    public double division (double a, double b) {
         return a / b;
     }
 }
--- src/test/java/tests/CalculatorDivDoubleTest.java
+++ src/test/java/tests/CalculatorDivDoubleTest.java
@@ -1,6 +1,8 @@
 package tests;
 
 import org.testng.Assert;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import utils.Retry;
 
@@ -10,62 +12,73 @@ public class CalculatorDivDoubleTest extends PreAndPostConditions {
 
     @Test (testName = "division with provider", dataProvider = "dataForDiv", dataProviderClass = StaticProvider.class)
     public void testDivision1(double a, double b, double expected) {
-        Assert.assertEquals(calculator.divDouble(a, b), expected, "неверное частное");
+        Assert.assertEquals(calculator.division(a, b), expected, "неверное частное");
     }
 
     @Test (testName = "division by |min|")
     public void testDivision2() {
-        Assert.assertEquals(calculator.divDouble(1, 10E-309), 1.0E308, "неверное частное");
+        Assert.assertEquals(calculator.division(1, 10E-309), 1.0E308, "неверное частное");
     }
 
     @Test (testName = "division by |min|-1")
     public void testDivision3() {
-        Assert.assertTrue(Double.isInfinite(calculator.divDouble(1, 10E-310)), "неверное частное");
+        Assert.assertTrue(Double.isInfinite(calculator.division(1, 10E-310)), "неверное частное");
     }
 
     @Test (testName = "division by -|min|")
     public void testDivision21() {
-        Assert.assertEquals(calculator.divDouble(1, -10E-309), -1.0E308, "неверное частное");
+        Assert.assertEquals(calculator.division(1, -10E-309), -1.0E308, "неверное частное");
     }
 
     @Test (testName = "division by -|min|-1")
     public void testDivision31() {
-        Assert.assertTrue(Double.isInfinite(calculator.divDouble(1, -10E-310)), "неверное частное");
+        Assert.assertTrue(Double.isInfinite(calculator.division(1, -10E-310)), "неверное частное");
     }
 
     @Test (testName = "division of |max|") //min-1 и max+1 idea не позволяет сделать
     public void testDivision4() {
-        Assert.assertEquals(calculator.divDouble(1.7e308, 1), 1.7E308, "неверное частное");
+        Assert.assertEquals(calculator.division(1.7e308, 1), 1.7E308, "неверное частное");
     }
 
     @Test (testName = "division of |max|+1")
     public void testDivision5() {
-        Assert.assertTrue(Double.isInfinite(calculator.divDouble(1.7e308, 0.1)), "неверное частное");
+        Assert.assertTrue(Double.isInfinite(calculator.division(1.7e308, 0.1)), "неверное частное");
     }
 
     @Test (testName = "division of -|max|") //min-1 и max+1 idea не позволяет сделать
     public void testDivision6() {
-        Assert.assertEquals(calculator.divDouble(-1.7e308, 1), -1.7E308, "неверное частное");
+        Assert.assertEquals(calculator.division(-1.7e308, 1), -1.7E308, "неверное частное");
     }
 
     @Test (testName = "division of -|max|-1", dependsOnMethods = "testDivision6", alwaysRun = true)
     public void testDivision7() {
-        Assert.assertTrue(Double.isInfinite(calculator.divDouble(-1.7e308, 0.1)), "неверное частное");
+        Assert.assertTrue(Double.isInfinite(calculator.division(-1.7e308, 0.1)), "неверное частное");
     }
 
     @Test (testName = "division by 0", groups = "division by zero")
     public void testDivisionByZero() {
-        Assert.assertTrue(Double.isInfinite(calculator.divDouble(1, 0)), "неверное частное");
+        Assert.assertTrue(Double.isInfinite(calculator.division(1.0, 0)), "неверное частное");
     }
 
     @Test (retryAnalyzer = Retry.class)
     public void retryTest() {
-        double x = calculator.divDouble(1, 0);
+        double x = calculator.division(1.0, 0);
         if (Double.isInfinite(x)) {
             System.out.println("Success");
             Assert.assertTrue(true);
         } else {
             Assert.assertTrue(false);
         }
+    }
+
+    @Test (testName = "NaN")
+    public void testNaN() {
+        Assert.assertTrue(Double.isNaN(calculator.division(0.0, 0)), "неверное частное");
+    }
+
+    @Parameters({"value1", "value2"})
+    @Test
+    public void paramTest(@Optional("1.0") Double value1, @Optional("1.0") Double value2) {
+        Assert.assertEquals(calculator.division(value1, value2), 1.0, "неверное частное");
     }
 }
